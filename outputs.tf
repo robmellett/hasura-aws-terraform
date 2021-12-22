2d4ac8c9e7007cb0e0ab9806a863e3f76a84a25c
Rob <dev@robmellett.com>
2021-12-22 10:30:16 +1000

    Output the ALB

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "ecs_security_group" {
   description = "Security group controlling access to the ECS tasks"
   value = aws_security_group.hasura_ecs
 }
+
+output "aws_alb" {
+  description = "The ALB location"
+  value = aws_alb.hasura
+}
